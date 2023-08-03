5ab0707b14369e06015e9537c1c88be7241a50a6
Santiago <sarozos@eafit.edu.co>
2023-08-03 12:23:21 -0500

    Agregar trabajo de clase Django

--- movie/views.py
+++ movie/views.py
@@ -8,3 +8,7 @@ def home(request):
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name':'Santiago Rozo'})
 
+def about(request):
+    return render(request, 'about.html')
+    
+
